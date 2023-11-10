0f6272837cc5e8039849403e9163ebbb90136a6e
Kostiantyn Borodach <37852364+Kostiantyn777@users.noreply.github.com>
2023-11-10 14:19:27 +0100

    Add functions to calculate basic math operations

---
+++ script.js
@@ -0,0 +1,31 @@
+function add(number1, number2) {
+  return number1 + number2;
+}
+
+function subtract(number1, number2) {
+  return number1 - number2;
+}
+
+function multiply(number1, number2) {
+  return number1 * number2;
+}
+function divide(number1, number2) {
+  return number1 / number2;
+}
+
+let firstNumber;
+let operator;
+let secondNumber;
+
+function operate(firstNumber, operator, secondNumber) {
+  if (operator === "+") {
+    return add(firstNumber, secondNumber);
+  } else if (operator === "-") {
+    return subtract(firstNumber, secondNumber);
+  } else if (operator === "*") {
+    return multiply(firstNumber, secondNumber);
+  } else if (operator === "/") {
+    return divide(firstNumber, secondNumber);
+  }
+}
+
