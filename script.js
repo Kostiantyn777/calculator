5461812ad74d865320578570d49452a83ec383e5
Kostiantyn Borodach <37852364+Kostiantyn777@users.noreply.github.com>
2023-11-13 19:57:43 +0100

    Add basic functionality to operate with two numbers

--- script.js
+++ script.js
@@ -1,21 +1,22 @@
 function add(number1, number2) {
-  return number1 + number2;
+  return parseInt(number1) + parseInt(number2);
 }
 
 function subtract(number1, number2) {
-  return number1 - number2;
+  return parseInt(number1) - parseInt(number2);
 }
 
 function multiply(number1, number2) {
-  return number1 * number2;
+  return parseInt(number1) * parseInt(number2);
 }
 function divide(number1, number2) {
-  return number1 / number2;
+  return parseInt(number1) / parseInt(number2);
 }
-let displayOnscreen = 0;
-let firstNumber;
-let operator;
-let secondNumber;
+let displayOnscreen = "0";
+
+let firstNumber = "";
+let operator = "";
+let secondNumber = "";
 
 function operate(firstNumber, operator, secondNumber) {
   if (operator === "+") {
@@ -31,13 +32,54 @@ function operate(firstNumber, operator, secondNumber) {
 
 const getScreen = document.querySelector(".screen");
 getScreen.innerText = displayOnscreen;
-;
-const allButtons = document.querySelectorAll("button");
 
-allButtons.forEach((button) => {
-  button.addEventListener("click", (e) => {
-    getScreen.innerText = e.target.innerText;
-    displayOnscreen = getScreen.innerText;
-    
+const allNumbers = document.querySelectorAll(".number-button");
+
+const allOperatorButtons = document.querySelectorAll(".operator-button");
+
+const dotButton = document.querySelector(".dot-button");
+const getClearButton = document.querySelector(".clear-button");
+const deleteButton = document.querySelector(".delete-button");
+
+allNumbers.forEach((number) => {
+  number.addEventListener("click", (e) => {
+    if (operator === "") {
+      firstNumber += e.target.innerText;
+      console.log(`First Number:${firstNumber}`);
+    } else {
+      secondNumber += e.target.innerText;
+      console.log(`Second Number:${secondNumber}`);
+    }
+  });
+});
+
+allOperatorButtons.forEach((operatorItem) => {
+  operatorItem.addEventListener("click", (e) => {
+    if (e.target.innerText !== "=") {
+      operator = e.target.innerText;
+
+      console.log(`Operator:${operator}`);
+    } else {
+      switch (operator) {
+        case "+":
+          console.log(operate(firstNumber, "+", secondNumber));
+          break;
+
+        case "-":
+          console.log(operate(firstNumber, "-", secondNumber));
+          break;
+
+        case "*":
+          console.log(operate(firstNumber, "*", secondNumber));
+          break;
+
+        case "/":
+          console.log(operate(firstNumber, "/", secondNumber));
+          break;
+
+        default:
+          break;
+      }
+    }
   });
 });
